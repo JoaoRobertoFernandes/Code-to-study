bc3d40819bc0e014fe23cbc01ebb39be338a8230
JoaoFernandes <joaofernandes4891@gmail.com>
2020-12-18 07:53:14 -0300

    Factorialize

---
+++ challenges/factorializa.js
@@ -0,0 +1,4 @@
+/*----------Factorialize----------
+const fac = (num) => {for (var res = 1; num > 0; num--){res *= num;}return res;}
+console.log(fac(5));        
+/*----------Factorialize----------*/
