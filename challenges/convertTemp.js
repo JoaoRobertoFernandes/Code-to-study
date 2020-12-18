0f67c21136845694b25256f56eec04d064543bde
JoaoFernandes <joaofernandes4891@gmail.com>
2020-12-18 07:25:24 -0300

    Celsius to fahrenheit.

---
+++ challenges/convertTemp.js
@@ -0,0 +1,7 @@
+/*----------Convert celsius to fahrenheit----------
+// f = fahrenheit c = celsius
+
+let f = 0; 
+const convert = (c) => f = (c * 9/5) + 32;
+console.log(convert(20)); //Return 68
+----------Convert celsius to fahrenheit----------*/
