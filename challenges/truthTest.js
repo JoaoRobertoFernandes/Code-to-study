0ac9df75d95ea17a91daab73630e32b2e389f99f
JoaoFernandes <joaofernandes4891@gmail.com>
2020-12-19 06:38:56 -0300

    Truth Test

---
+++ challenges/truthTest.js
@@ -0,0 +1,4 @@
+/*----------Truth Test----------
+const check = (arr, func) => arr.find(func);
+console.log(check([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; }))//return 8
+/*----------Truth Test----------*/
