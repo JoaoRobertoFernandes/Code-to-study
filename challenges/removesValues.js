2ec1575d1c313b5c3b75e10d0bc3346313dd32f6
JoaoFernandes <joaofernandes4891@gmail.com>
2020-12-19 07:36:38 -0300

    Remove Values

---
+++ challenges/removesValues.js
@@ -0,0 +1,4 @@
+/*-----------Remove values----------
+const val = (arr) => arr.filter(Boolean);
+console.log(val([7, "ate", "", false, 9]));//return [ 7, 'ate', 9 ]
+/*-----------Remove values----------*/
