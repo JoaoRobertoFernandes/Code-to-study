23e3351a2a571cd5852094676684f7a6063a5ac8
JoaoFernandes <joaofernandes4891@gmail.com>
2020-12-19 07:06:25 -0300

    Title case

---
+++ challenges/titleCase.js
@@ -0,0 +1,5 @@
+/*----------Title Case----------
+const cas = (str) => str.toLowerCase().replace(/(^|\s)\S/g, L => L.toUpperCase());
+
+console.log(cas("I'm a little tea pot"));//return I'm A Little Tea Pot
+/*----------Title Case----------*/
