6fad6428f096558caf8d107f0e74765b95cdbdb2
JoaoFernandes <joaofernandes4891@gmail.com>
2020-12-19 05:20:20 -0300

    String end

---
+++ challenges/stringEnd.js
@@ -0,0 +1,5 @@
+/*----------Confirm the end of String----------
+
+const check = (s, n) => s.slice(s.length - n.length) === n;
+console.log(check("Dog", "g"));
+/*----------Confirm the end of String----------*/
