25654910f5a081835e6af8295c5f45f86a812ce6
JoaoFernandes <joaofernandes4891@gmail.com>
2020-12-19 05:54:23 -0300

    Repeat String

---
+++ challenges/repeatString.js
@@ -0,0 +1,10 @@
+/*----------Repeat String-----------
+const rep = (s, n) => {
+    let res = "";
+    for(let i = 0; i < n; i++) {
+        res += s;
+    }
+    return res;
+}
+console.log(rep("k", 3));
+/*----------Repeat String-----------*/
