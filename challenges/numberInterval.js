c81f81965a543c69947c13c6140d781b04520408
JoaoFernandes <joaofernandes4891@gmail.com>
2020-12-19 08:02:53 -0300

    Number interval

---
+++ challenges/numberInterval.js
@@ -0,0 +1,6 @@
+/*---------Number Interval----------
+const check = (arr, num) => {
+    return arr.filter(val => num > val).length;
+}
+console.log(check([10, 20, 30, 40, 50], 35));
+/*---------Number Interval----------*/
