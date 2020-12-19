47603384f999d8cff24c578ead1ac99373147a02
JoaoFernandes <joaofernandes4891@gmail.com>
2020-12-19 06:47:57 -0300

    Boolean check

---
+++ challenges/booleanCheck.js
@@ -0,0 +1,6 @@
+/*---------Boolean check---------
+const check = (x) => (typeof x == "boolean");
+console.log(check(2));//return false
+console.log(check(false));//return true
+
+/*---------Boolean check---------*/ 
