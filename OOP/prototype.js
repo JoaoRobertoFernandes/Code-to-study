6c4706bf3a9e08b78c56ff310cc34891aa93da3a
JoaoFernandes <joaofernandes4891@gmail.com>
2020-12-21 16:27:08 -0300

    Prototype

--- OOP/prototype.js
+++ OOP/prototype.js
@@ -7,11 +7,18 @@ function Dog(name) {
 Dog.prototype = {
     constructor: Dog,
     numLegs: 4,
-    eat: () => console.log("Eating"),
+    //eat: () => console.log("Eating"),
 }
-let beagle = new Dog("Jo");
-console.log(Dog.prototype.eat);//return [Function: eat]
+
+//DRY
+function Eat() {}
+Eat.prototype.eat = () => console.log("Eating");
+
+let beagle = Object.create(Eat.prototype);
+//console.log(Dog.prototype.eat);//return [Function: eat]
 
 //Using isPrototypeOf() to check for inherits 
-console.log(Dog.prototype.isPrototypeOf(beagle));//return true
+//console.log(Dog.prototype.isPrototypeOf(beagle));//return true
+console.log(beagle.eat());
+
 /*----------Prototype-----------*/
