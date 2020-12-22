a6740cff7895c04f4635da87522d30e934e627a4
JoaoFernandes <joaofernandes4891@gmail.com>
2020-12-22 05:43:47 -0300

    Slice to return a part of an array

---
+++ challenges/usingSlice.js
@@ -0,0 +1,7 @@
+/*----------Slice to return a part of an array-----------
+var  arr = ["a", "b", "c", "d"];
+
+const sliceArr = (array, begin, end) => array.slice(begin, end);
+
+console.log(sliceArr(arr, 0, 2));//return [ 'a', 'b' ]
+/*----------Slice to return a part of an array-----------*/
