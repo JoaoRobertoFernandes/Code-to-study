b5505c61911937c210b3b457245c59ddb132d996
JoaoFernandes <joaofernandes4891@gmail.com>
2021-01-23 21:22:22 -0300

    Tuples and Lists

---
+++ Python/inputFromUsers.py
@@ -0,0 +1,19 @@
+#Input
+
+'''
+#Lists != Tuples
+#------------------------#
+print("----------------------")
+#rate = (1, 2, 3) tuple cannot be changed 
+rate = [1, 2, 3] #lists can be changed 
+print(rate[1]) 
+rate[1] = 3
+print(rate[1])
+print("----------------------")
+#------------------------#
+'''
+
+'''
+#Tuples lists 
+rate = [(1, 2), (3,4)]
+'''
