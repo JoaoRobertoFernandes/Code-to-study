78987356d217d2ddaed8f87f10f226713e4c201b
JoaoFernandes <joaofernandes4891@gmail.com>
2021-01-25 21:08:00 -0300

    Dictionaries

---
+++ Python/dictionaries.py
@@ -0,0 +1,13 @@
+#Dictionaries
+'''
+#---------------------------------#
+print("---------------------------")
+second_name = {
+    "João": "Roberto",
+    "Fernandes": "Oliveira"
+}
+
+print(changing_names.get("João"))
+print("---------------------------")
+#---------------------------------#
+'''
